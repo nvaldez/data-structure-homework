f7b4d76cf425a51b533e8bb73e9172a073aafc66
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2020-05-11 10:33:55 -0500

    part one of the home work

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // part 1 of the home work
+        int nums[] = {1,2,3,4,5};
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++){
+
+            sum = sum + nums[i];
+        }
+        System.out.println("The sum is: " + sum);
+    }
+}
