aa2b2246da5a2108c21ef9b34382fb5c1600ab49
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2020-05-11 13:36:39 -0500

    completed part 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // part 1 of the home work
+        // part 1 of the homework
         int nums[] = {1,2,3,4,5};
         int sum = 0;
 
@@ -10,6 +12,32 @@ public class Main {
 
             sum = sum + nums[i];
         }
-        System.out.println("The sum is: " + sum);
+        System.out.println("\nThe sum for part one is: " + sum);
+
+        //part 2 of the homework
+        // the output for part 2 of the homework is 2. exampleArray only goes into the
+        // if statement twice.
+
+        System.out.println("\ntoPower static method output: " + toPower(4, 2));
+    }
+
+    // part 3 fo the homework
+    public static ArrayList<Integer> toPower(int size, int power){
+        int total = 0;
+
+        ArrayList<Integer> power2 =  new ArrayList<Integer>();
+
+        for (int i = 0; i < size; i++){
+
+                total = i;
+            for (int j = 1; j < power; j++){
+
+                total = total * i;
+            }
+
+            power2.add(total);
+        }
+
+        return power2;
     }
 }
